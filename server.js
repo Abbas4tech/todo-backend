e215e6c0a6d2efd10df68d8e8d4b2e993bfbecdd
Abbas4tech <shaikhabbas54@gmail.com>
2022-07-30 12:25:43 +0530

    databse connect

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/My-Todos", (err) => {
+  if (err) console.log("Error occured");
+  else console.log("Database connected");
+});
